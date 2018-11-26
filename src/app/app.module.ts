e30052e335c9e6d7b9ecebd1524b04a164a87fc7
pierrepoulouin <pierrepoulouin@gmail.com>
2018-11-26 17:43:42 +0100

    page d'accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,MDBBootstrapModule.forRoot()
